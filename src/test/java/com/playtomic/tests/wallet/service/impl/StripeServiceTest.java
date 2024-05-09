cc32a4ce14274627a4bea9b7b0bb21c06c19a598
Antonio D'Alessio <antonio.dalessio@treatwell.com>
2024-05-09 10:10:01 +0200

    wiremock and mockserver attempts

--- src/test/java/com/playtomic/tests/wallet/service/impl/StripeServiceTest.java
+++ src/test/java/com/playtomic/tests/wallet/service/impl/StripeServiceTest.java
@@ -1,31 +1,72 @@
 package com.playtomic.tests.wallet.service.impl;
 
 
-import com.playtomic.tests.wallet.service.stripe.StripeAmountTooSmallException;
-import com.playtomic.tests.wallet.service.stripe.StripeServiceException;
-import com.playtomic.tests.wallet.service.stripe.StripeService;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 import java.math.BigDecimal;
 import java.net.URI;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockserver.client.MockServerClient;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.testcontainers.containers.MockServerContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import com.playtomic.tests.wallet.service.stripe.StripeAmountTooSmallException;
+import com.playtomic.tests.wallet.service.stripe.StripeService;
+import com.playtomic.tests.wallet.service.stripe.StripeServiceException;
+
 
 /**
  * This test is failing with the current implementation.
  *
  * How would you test this?
  */
+
 public class StripeServiceTest {
 
-    URI testUri = URI.create("http://how-would-you-test-me.localhost");
-    StripeService s = new StripeService(testUri, testUri, new RestTemplateBuilder());
+    private static final DockerImageName MOCKSERVER_IMAGE = DockerImageName
+            .parse("mockserver/mockserver")
+            .withTag("mockserver-" + StripeServiceTest.class.getPackage().getImplementationVersion());
+
+    private static MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE);
+    private URI TEST_URI = URI.create(mockServer.getHost());
+
+    StripeService s = new StripeService(TEST_URI, TEST_URI, new RestTemplateBuilder());
+
+    @BeforeAll
+    static void beforeAll() {
+        mockServer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mockServer.stop();
+    }
 
     @Test
     public void test_exception() {
+        String creditCardNumber = "4242 4242 4242 4242";
+        int amount = 5;
+        try (MockServerClient mockServerClient = new MockServerClient("localhost", 9999)) {
+            mockServerClient.when(
+                                    request()
+                                            .withMethod("POST")
+                                            .withBody("{\"credit_card\": \"%s\", \"amount\": %d}".formatted(creditCardNumber, amount))
+                            )
+                            .respond(
+                                    response()
+                                            .withStatusCode(422)
+                            );
+        }
+
         Assertions.assertThrows(StripeAmountTooSmallException.class, () -> {
-            s.charge("4242 4242 4242 4242", new BigDecimal(5));
+            s.charge(creditCardNumber, new BigDecimal(amount));
         });
     }
 
